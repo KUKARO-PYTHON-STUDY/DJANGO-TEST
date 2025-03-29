7fa706b9b236f225407a28314217e99a66973458
justk <justkukaro@naver.com>
2025-03-29 16:19:20 +0900

    feat:DB변화

--- pylog/blog/views.py
+++ pylog/blog/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    return render(request, "post_list.html")
+    posts = Post.objects.all()
+    context = {
+        "posts": posts,
+    }
+    return render(request, "post_list.html", context)

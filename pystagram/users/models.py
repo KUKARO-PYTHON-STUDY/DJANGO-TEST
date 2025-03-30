94782f6c9c5f3caab269c10a5c65be3e5d6822bc
justk <justkukaro@naver.com>
2025-03-30 20:35:24 +0900

    feat: admin user 추가

--- pystagram/users/models.py
+++ pystagram/users/models.py
@@ -1,6 +1,11 @@
-# from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    profile_image = models.ImageField(
+        "프로필 이미지",
+        upload_to="users/profile",
+        blank=True,
+    )
+    short_description = models.TextField("소개글", blank=True)
